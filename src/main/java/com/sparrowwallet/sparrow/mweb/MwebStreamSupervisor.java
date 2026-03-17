283b1e68a441ec784a5775d0f10878c1c0e3ee52
Hector Chu <hectorchu@gmail.com>
2026-03-17 19:54:05 +0000

    Add mweb txn and utxo

--- src/main/java/com/sparrowwallet/sparrow/mweb/MwebStreamSupervisor.java
+++ src/main/java/com/sparrowwallet/sparrow/mweb/MwebStreamSupervisor.java
@@ -2,24 +2,30 @@ package com.sparrowwallet.sparrow.mweb;
 
 import com.google.common.eventbus.Subscribe;
 import com.google.protobuf.ByteString;
+import com.sparrowwallet.drongo.address.Address;
+import com.sparrowwallet.drongo.address.InvalidAddressException;
+import com.sparrowwallet.drongo.address.MwebAddress;
+import com.sparrowwallet.drongo.protocol.Script;
 import com.sparrowwallet.drongo.protocol.ScriptType;
-import com.sparrowwallet.drongo.wallet.Keystore;
-import com.sparrowwallet.drongo.wallet.Wallet;
+import com.sparrowwallet.drongo.protocol.Sha256Hash;
+import com.sparrowwallet.drongo.protocol.Transaction;
+import com.sparrowwallet.drongo.wallet.*;
 import com.sparrowwallet.sparrow.AppServices;
 import com.sparrowwallet.sparrow.EventManager;
 import com.sparrowwallet.sparrow.event.OpenWalletsEvent;
+import com.sparrowwallet.sparrow.event.WalletHistoryChangedEvent;
+import com.sparrowwallet.sparrow.io.Storage;
 import com.sparrowwallet.sparrow.mweb.proto.RpcGrpc;
 import com.sparrowwallet.sparrow.mweb.proto.Utxo;
 import com.sparrowwallet.sparrow.mweb.proto.UtxosRequest;
 import io.grpc.Context;
 import io.grpc.Status;
 import io.grpc.stub.StreamObserver;
+import javafx.application.Platform;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
+import java.util.*;
 
 public class MwebStreamSupervisor {
     private static final Logger log = LoggerFactory.getLogger(MwebStreamSupervisor.class);
@@ -29,8 +35,8 @@ public class MwebStreamSupervisor {
 
     public MwebStreamSupervisor(RpcGrpc.RpcStub stub) {
         this.stub = stub;
-        for (var wallet : AppServices.get().getOpenWallets().keySet()) {
-            addWallet(wallet);
+        for (var entry : AppServices.get().getOpenWallets().entrySet()) {
+            addWallet(entry.getKey(), entry.getValue());
         }
         EventManager.get().register(this);
     }
@@ -44,27 +50,27 @@ public class MwebStreamSupervisor {
 
     @Subscribe
     public void openWallets(OpenWalletsEvent event) {
-        var openWallets = new HashSet<>(event.getWallets());
+        var openWallets = event.getWalletsMap();
         var iterator = wallets.entrySet().iterator();
         while (iterator.hasNext()) {
             var entry = iterator.next();
-            if (!openWallets.contains(entry.getKey())) {
+            if (!openWallets.containsKey(entry.getKey())) {
                 entry.getValue().cancel(null);
                 iterator.remove();
             }
         }
-        for (var wallet : openWallets) {
-            addWallet(wallet);
+        for (var entry : openWallets.entrySet()) {
+            addWallet(entry.getKey(), entry.getValue());
         }
     }
 
-    private void addWallet(Wallet wallet) {
+    private void addWallet(Wallet wallet, Storage storage) {
         if (wallets.containsKey(wallet)) return;
         if (wallet.getScriptType() != ScriptType.MWEB) return;
-        wallets.put(wallet, startStream(wallet));
+        wallets.put(wallet, startStream(wallet, storage));
     }
 
-    private Context.CancellableContext startStream(Wallet wallet) {
+    private Context.CancellableContext startStream(Wallet wallet, Storage storage) {
         Keystore keystore = wallet.getKeystores().getFirst();
         int height = 0;
         for (var txn : wallet.getTransactions().values()) {
@@ -79,7 +85,32 @@ public class MwebStreamSupervisor {
         context.run(() -> stub.utxos(request, new StreamObserver<>() {
             @Override
             public void onNext(Utxo utxo) {
-                System.out.println(utxo);
+                Address address;
+                try {
+                    address = MwebAddress.fromString(utxo.getAddress());
+                } catch (InvalidAddressException _) {
+                    return;
+                }
+                WalletNode affectedNode = null;
+                for (var purposeNode : wallet.getPurposeNodes()) {
+                    for (var node : purposeNode.getChildren()) {
+                        if (node.getAddress().equals(address)) {
+                            affectedNode = node;
+                            break;
+                        }
+                    }
+                    if (affectedNode != null) break;
+                }
+                if (affectedNode == null) return;
+                var tx = new Transaction();
+                tx.addInput(Sha256Hash.wrap(utxo.getOutputId()), 0, new Script(List.of()));
+                tx.addOutput(utxo.getValue(), address);
+                var txn = new BlockTransaction(tx.getTxId(), utxo.getHeight(), new Date(utxo.getBlockTime() * 1000L), 0L, tx);
+                var txnhi = new BlockTransactionHashIndex(txn.getHash(), txn.getHeight(), txn.getDate(), txn.getFee(), 0, utxo.getValue());
+                wallet.updateTransactions(Map.of(txn.getHash(), txn));
+                affectedNode.updateTransactionOutputs(wallet, Set.of(txnhi));
+                var event = new WalletHistoryChangedEvent(wallet, storage, List.of(affectedNode), List.of());
+                Platform.runLater(() -> EventManager.get().post(event));
             }
 
             @Override
