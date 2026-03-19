c5693b7f235ebcd004cafb98fa27718a637e810e
Hector Chu <hectorchu@gmail.com>
2026-03-19 12:23:19 +0000

    Refactor

--- src/main/java/com/sparrowwallet/sparrow/mweb/MwebFeeEstimator.java
+++ src/main/java/com/sparrowwallet/sparrow/mweb/MwebFeeEstimator.java
@@ -1,61 +1,35 @@
 package com.sparrowwallet.sparrow.mweb;
 
-import com.google.protobuf.ByteString;
-import com.sparrowwallet.drongo.KeyPurpose;
-import com.sparrowwallet.drongo.crypto.ECKey;
-import com.sparrowwallet.drongo.protocol.*;
+import com.sparrowwallet.drongo.protocol.TransactionInput;
+import com.sparrowwallet.drongo.protocol.TransactionOutPoint;
+import com.sparrowwallet.drongo.protocol.TransactionOutput;
 import com.sparrowwallet.drongo.wallet.*;
-import com.sparrowwallet.sparrow.mweb.proto.CreateRequest;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MwebFeeEstimator implements FeeEstimator {
     @Override
-    public long calcFeeIncrease(Wallet wallet, Map<BlockTransactionHashIndex, WalletNode> selectedUtxos,
+    public long calcFeeIncrease(Wallet wallet, Map<BlockTransactionHashIndex, WalletNode> utxos,
                                 List<WalletTransaction.Output> outputs, double feeRate) {
-        if (wallet.getScriptType() != ScriptType.MWEB) return 0;
-        ECKey scanKey = wallet.getKeystores().getFirst().getMwebScanPrivateKey();
-        Transaction tx = new Transaction();
-        for (var entry : selectedUtxos.entrySet()) {
-            var prevTx = wallet.getWalletTransaction(entry.getKey().getHash()).getTransaction();
-            int index = prevTx.getOutputs().stream()
-                    .filter(out -> ScriptType.MWEB.isScriptType(out.getScript()))
-                    .toList().indexOf(prevTx.getOutputs().get((int)entry.getKey().getIndex()));
-            int addressIndex = 0;
-            if (entry.getValue().getKeyPurpose() == KeyPurpose.RECEIVE) {
-                addressIndex = entry.getValue().getDerivation().getLast().i() + 1;
+        var tx = MwebServer.get().create(wallet, utxos, outputs, feeRate, true);
+        var ins = tx.getInputs().stream().map(TransactionInput::getOutpoint).collect(Collectors.toSet());
+        long sumIn = 0, mwebIn = 0;
+        for (var utxo : utxos.keySet()) {
+            if (ins.contains(new TransactionOutPoint(utxo.getHash(), utxo.getIndex()))) {
+                sumIn += utxo.getValue();
+            } else {
+                mwebIn += utxo.getValue();
             }
-            tx.addInput(prevTx.getMwebOutputId(index), addressIndex, new Script(List.of()));
         }
-        for (var output : outputs) {
-            var out = output.getTransactionOutput();
-            if (ScriptType.MWEB.isScriptType(out.getScript())) {
-                var pubKeys = ScriptType.MWEB.getHashFromScript(out.getScript());
-                out = new TransactionOutput(tx, out.getValue(), pubKeys);
-            }
-            tx.addOutput(out);
-        }
-        var resp = MwebServer.get().stub.create(CreateRequest.newBuilder()
-                .setRawTx(ByteString.copyFrom(tx.bitcoinSerialize()))
-                .setScanSecret(ByteString.copyFrom(scanKey.getPrivKeyBytes()))
-                .setSpendSecret(ByteString.copyFrom(new byte[32]))
-                .setFeeRatePerKb((long)Math.ceil(feeRate * 1000))
-                .setDryRun(true)
-                .build());
-        var tx2 = new Transaction(resp.getRawTx().toByteArray());
-        long sumIn1 = selectedUtxos.keySet().stream().mapToLong(BlockTransactionHashIndex::getValue).sum();
-        long sumIn2 = tx2.getInputs().stream().mapToLong(in ->
-                wallet.getWalletTransaction(in.getOutpoint().getHash()).getTransaction()
-                        .getOutputs().get((int)in.getOutpoint().getIndex()).getValue()).sum();
-        long sumOut1 = tx.getOutputs().stream().mapToLong(TransactionOutput::getValue).sum();
-        long sumOut2 = tx2.getOutputs().stream().mapToLong(TransactionOutput::getValue).sum();
-        long mwebIn = sumIn1 - sumIn2;
+        long sumOut1 = outputs.stream().mapToLong(out -> out.getTransactionOutput().getValue()).sum();
+        long sumOut2 = tx.getOutputs().stream().mapToLong(TransactionOutput::getValue).sum();
         long expectedPegIn = Math.max(0, sumOut1 - mwebIn);
         long feeIncrease = sumOut2 - expectedPegIn;
         if (expectedPegIn > 0) {
             feeIncrease += (long)Math.ceil(feeRate * 41);
         }
-        return feeIncrease;
+        return sumIn < sumOut2 ? feeIncrease : 0;
     }
 }
